6bc8338d5ca220ccfe53a1369eba63c85eeb14be
emilw <emil@postback.se>
2016-11-19 13:37:50 +0100

    Introduce central config for all registered deployments for a client

--- go.py
+++ go.py
@@ -1,14 +1,28 @@
 import os
 import git
 import json
+import datetime
+import urllib2
 
 class Config(object):
     def __init__(self, jsonData):
         self.__dict__ = jsonData
 
+class DeployConfig:
+    def __init__(self, sourceURL, created, updated, versiontag):
+        self.SourceURL = sourceURL
+        self.Created = created.isoformat()
+        self.Updated = updated.isoformat()
+        self.VersionTag = versiontag
+def jdefault(o):
+    return o.__dict__
 
 def log(text):
     print(text)
+
+def writeJson(object, path):
+    with open(path, 'w') as fp:
+        json.dump(object, fp, default=jdefault, indent=4)
 
 def createDeploymentIfNotExists(gitURL):
     localFolder = gitURL.split("/")[-1].split(".")[0]
@@ -21,7 +35,7 @@ def createDeploymentIfNotExists(gitURL):
         log("...Done")
     else:
         log("The deployment do not exists, downloading....")
-        git.Repo.clone_from(gitURL, localFolder)
+        git.Repo.clone_from(gitURL+".git", localFolder)
         log("....Done")
         #os.makedirs(localFolder)
     
@@ -35,12 +49,34 @@ def getConfiguration(deployPath):
     conf = Config(data)
     return conf
 
-def setupConfig(conf):
+def setupConfig(conf, gitURL):
     log("Starting to configure: " + conf.name)
-    #log("Number of python pre reqs: " + len(conf.prerequisitesPython))
-log("Starting deployment")
+    home = os.path.expanduser("~")
+    deployConfPath = home+"/.deploy"
+    deployConfigs = []
+    if(not os.path.exists(deployConfPath)):
+        log(".deploy will be created")
+        d = DeployConfig(gitURL, datetime.datetime.now(),datetime.datetime.now(),"")
 
-deployPath = createDeploymentIfNotExists("https://github.com/emilw/deployTest.git")
+        deployConfigs.append(d)
+        writeJson(deployConfigs, deployConfPath)
+    else:
+        deployConfigs = json.load(deployConfPath)
+    
+
+gitURL = "https://github.com/emilw/deployTest"
+
+
+try:
+    deployJsonURL = gitURL+"/blob/master/deploy.json"
+    status = urllib2.urlopen(deployJsonURL).getcode()
+except urllib2.HTTPError:
+    log("Failed to find deploy.json in path, please verify path %s in browser" % (deployJsonURL))
+    exit()
+
+deployPath = createDeploymentIfNotExists(gitURL)
 conf = getConfiguration(deployPath)
 
-setupConfig(conf)
+setupConfig(conf, gitURL)
+
+log("All done!")
